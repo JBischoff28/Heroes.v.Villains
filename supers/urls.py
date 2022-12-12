d067600bd70dba99eea4721b6efdec69826d4e8a
Joshua Bischoff <joshuabischoff1@gmail.com>
2022-12-12 12:11:08 -0500

    Finished Developing Functionality For A Method That Returns All Supers With The Option To Filter For Heroes Or Villains.

--- supers/urls.py
+++ supers/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from supers import views
 
 urlpatterns = [
-    path('', views.supers_list)
+    path('', views.supers_list),
 
 ]
--- supers/views.py
+++ supers/views.py
@@ -9,20 +9,18 @@ from .serializers import SuperSerializer
 
 @api_view(['GET', 'POST'])
 def supers_list(request):
-
+    
     if request.method == 'GET':
-
         type_name = request.query_params.get('type')
         print(type_name)
 
         supers_query = Super.objects.all()
 
         if type_name:
-            supers_query = supers_query.filter(super_type__name=type_name)
+            supers_query = supers_query.filter(super_type__type=type_name)
         
         serializer = SuperSerializer(supers_query, many=True)
         return Response(serializer.data)
-
 
     if request.method == 'POST':
         pass
