d3101c9d7fa188b1168dbed803da4a7c348fffc9
Joshua Bischoff <joshuabischoff1@gmail.com>
2022-12-12 22:00:01 -0500

    Created A Dictionary To Serve As A Custom Response Method.

--- supers/views.py
+++ supers/views.py
@@ -12,15 +12,26 @@ def supers_list(request):
     
     if request.method == 'GET':
         type_name = request.query_params.get('type')
-        print(type_name)
 
         supers_query = Super.objects.all()
+        heroes_query = Super.objects.filter(super_type=1)
+        villains_query = Super.objects.filter(super_type=2)
 
         if type_name:
             supers_query = supers_query.filter(super_type__type=type_name)
         
-        serializer = SuperSerializer(supers_query, many=True)
-        return Response(serializer.data)
+        hero_serializer = SuperSerializer(heroes_query, many=True)
+        villains_serializer = SuperSerializer(villains_query, many=True)
+        
+        custom_response = {
+            'Heroes': [hero_serializer.data],
+            'Villains': [villains_serializer.data]
+        }
+        
+        return Response(custom_response)
 
-    if request.method == 'POST':
-        pass
+    elif request.method == 'POST':
+        serializer = SuperSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
